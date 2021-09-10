000bd8371ee6e048981dce424f7d38062cad0f56
Ashok Kumar <59107244+ashok148@users.noreply.github.com>
2021-09-11 02:05:25 +0530

    Add files via upload

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,1 +1,16 @@
+import React from "react";
+import Tilt from 'react-tilt'
+import './Logo.css';
+import brain from './brain.png';
 
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner"> <img style={{width:'100px', paddingTop:'20px'}} alt='logo' src={brain}/> </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
