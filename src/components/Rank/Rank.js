6fbf0d3845e5534619fb59c10bc85d1f50b6b229
Ashok Kumar <59107244+ashok148@users.noreply.github.com>
2021-09-11 02:06:24 +0530

    Add files via upload

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,1 +1,16 @@
+import React from "react";
+// import './Rank.css';
+const Rank = () => {
+    return (
+        <div>
+           <div className='white f3'>
+               {'Hey! Ashok, your current rank is...'}
+           </div>
+           <div className='white f1'>
+               {'#5'}
+           </div>
+        </div>
+    );
+}
 
+export default Rank;
